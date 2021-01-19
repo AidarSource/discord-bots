07e2db2d02225c05afd846a830ee339dfd7e4297
AidarSource <34319725+AidarSource@users.noreply.github.com>
2021-01-20 00:52:08 +0600

    add current_map() function

--- JAZE-bot/algorithms/serverStatus.py
+++ JAZE-bot/algorithms/serverStatus.py
@@ -27,3 +27,16 @@ def max_players(url):
     # grabs max_players of the server
     maxZE_players = page_soup.find("span", id="HTML_max_players")
     return maxZE_players.text
+
+# returns current map of the server. ONLY GAMETRACKER URL ALLOWED
+def current_map(url):
+    my_url = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    # Opening up connection, grabbing the page
+    uClient = uReq(my_url)
+    page_html = uClient.read()
+    uClient.close()
+    # html parser
+    page_soup = soup(page_html, "html.parser")
+    # grabs max_players of the server
+    currentMap = page_soup.find("div", id="HTML_curr_map")
+    return currentMap.text.strip()
